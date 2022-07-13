ec2b04774a8e62a1214722990bde36cd741079e7
CrocoXXX <lawrencenoman@gmail.com>
2022-07-13 22:11:41 +0700

    finish web

---
+++ lib/pages/website.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web/theme.dart';
+
+class Website extends StatefulWidget {
+  const Website({Key? key}) : super(key: key);
+
+  @override
+  State<Website> createState() => _WebsiteState();
+}
+
+class _WebsiteState extends State<Website> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget navigation({required String title, required int index}) {
+      return InkWell(
+        onTap: () {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  title,
+                  style: selectedIndex == index ? klik1 : klik2,
+                ),
+                Container(
+                  width: 66,
+                  height: 2,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color:
+                        selectedIndex == index ? lightRed : Colors.transparent,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 50,
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Image.asset(
+                "images/background.png",
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                fit: BoxFit.fill,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 100,
+                  vertical: 30,
+                ),
+                child: Column(
+                  children: [
+                    // NOTE: NAVIGATION
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset(
+                          "images/logo.png",
+                          width: 70,
+                          height: 40,
+                        ),
+                        Row(
+                          children: [
+                            navigation(
+                              title: "Guides",
+                              index: 0,
+                            ),
+                            navigation(
+                              title: "Pricing",
+                              index: 1,
+                            ),
+                            navigation(
+                              title: "Team",
+                              index: 2,
+                            ),
+                            navigation(
+                              title: "Stories",
+                              index: 3,
+                            ),
+                          ],
+                        ),
+                        Image.asset(
+                          "images/button.png",
+                          width: 163,
+                          height: 53,
+                        ),
+                      ],
+                    ),
+                    // NOTE: CONTENT
+                    const SizedBox(
+                      height: 76,
+                    ),
+                    Image.asset(
+                      "images/illustration.png",
+                      width: 550,
+                      // width: 763,
+                      // height: 550,
+                    ),
+
+                    // NOTE: FOOTER
+                    const SizedBox(
+                      height: 84,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "images/icon_scroll.png",
+                          width: 24,
+                          height: 24,
+                        ),
+                        const SizedBox(
+                          width: 13,
+                        ),
+                        Text(
+                          "Scroll to Learn More",
+                          style: bottom,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color white = const Color(0xffFFFFFF);
+Color darkBlue = const Color(0xff1D1E3C);
+Color lightRed = const Color(0xffFE998D);
+Color black = const Color(0xff000000);
+
+TextStyle klik1 = GoogleFonts.poppins(
+  color: darkBlue,
+  fontWeight: FontWeight.w500,
+  fontSize: 18,
+);
+
+TextStyle klik2 = GoogleFonts.poppins(
+  color: darkBlue,
+  fontWeight: FontWeight.w300,
+  fontSize: 18,
+);
+
+TextStyle account = GoogleFonts.poppins(
+  color: white,
+  fontWeight: FontWeight.w500,
+  fontSize: 18,
+);
+
+TextStyle bottom = GoogleFonts.poppins(
+  color: black,
+  fontWeight: FontWeight.w400,
+  fontSize: 20,
+);
